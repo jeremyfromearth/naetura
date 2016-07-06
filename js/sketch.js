c1879dbec5e7893825a68e86414e21eed9a9f92a
jeremyfromearth <jeremynealbrown@gmail.com>
2016-07-06 11:47:45 -0700

    Opted for trackball over orbit control, added method to enable full window display

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,41 @@
 var Sketch = function(width, height) {
-    var renderCallback = null;
+    var renderCallback = function(){};
     var scene = new THREE.Scene();
     var aspect = width / height;
     var camera = new THREE.PerspectiveCamera(75, aspect, 1, 1000);
     var renderer = new THREE.WebGLRenderer();
-    var controls = new THREE.OrbitControls(camera, renderer.domElement);
     renderer.setSize(width, height);
-    controls.enabled = false;
+
+    var controls = new THREE.TrackballControls(camera);
+    controls.rotateSpeed = 5.0;
+    controls.zoomSpeed = 1.0;
+    controls.panSpeed = 5.0;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.01;
 
     function render() {
-        requestAnimationFrame(render);        
-        if(renderCallback) renderCallback();
+        renderCallback();
+        controls.update()
         renderer.render(scene, camera);
+        requestAnimationFrame(render);        
+    }
+
+    function setFullWindow(isFull) {
+        if(isFull) {
+            window.addEventListener( 'resize', onWindowResize, false );
+            onWindowResize();
+        } else {
+            window.removeEventListener( 'resize', onWindowResize, false );
+        }
+    }
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        controls.handleResize();
     }
 
     return {
@@ -22,6 +46,7 @@ var Sketch = function(width, height) {
         controls : controls,
         setRenderCallback : function(callback) {
             renderCallback = callback;
-        }
+        }, 
+        setFullWindow : setFullWindow
     }
 }
