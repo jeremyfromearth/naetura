66b30aa9caeb39e95686bef740d1329ca62535da
jeremyfromearth <jeremynealbrown@gmail.com>
2016-07-06 10:04:47 -0700

    Added ability to enable orbit and zoom control in sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -4,7 +4,9 @@ var Sketch = function(width, height) {
     var aspect = width / height;
     var camera = new THREE.PerspectiveCamera(75, aspect, 1, 1000);
     var renderer = new THREE.WebGLRenderer();
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
     renderer.setSize(width, height);
+    controls.enabled = false;
 
     function render() {
         requestAnimationFrame(render);        
@@ -17,6 +19,7 @@ var Sketch = function(width, height) {
         camera : camera,
         renderer : renderer,
         render : render,
+        controls : controls,
         setRenderCallback : function(callback) {
             renderCallback = callback;
         }
