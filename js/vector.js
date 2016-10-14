a62457392f2a99d8d780b37ee1a7139071e17d2d
jeremyfromearth <jeremynealbrown@gmail.com>
2016-10-13 19:59:10 -0700

    Added Vec2, renamed Vector to Vec3

--- js/vector.js
+++ js/vector.js
@@ -1,12 +1,62 @@
-function Vector(x, y, z) {
-    console.log(x, y, z);
+function Vec2(x, y) {
+    return {
+        x : x || 0,
+        y : y || 0,
+
+        add : function(v) {
+            return Vec2(this.x + v.x, this.y + v.y);
+        },
+
+        distance : function(v) {
+            var dx, dy;
+            dx = this.x - v.x;
+            dy = this.y - v.y;
+            return Math.sqrt(dx * dx + dy * dy);
+        },
+
+        divide : function(n) {
+            return Vec2(this.x / n, this.y / n);
+        },
+
+        dot : function(v) {
+            return this.x * v.x + this.y * v.y;
+        },
+
+        length : function() {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        },
+
+        magnitude : function() {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        },
+
+        normalized : function() {
+            var len = this.length();
+            if (len === 0.0) {
+                return Vec2();
+            } else {
+                return this.divide(len);
+            }
+        },
+
+        scale : function(n) {
+            return Vec2(x * n, y * n);
+        },
+
+        subtract : function(v) {
+            return Vec2(this.x - v.x, this.y - v.y);
+        } 
+    }
+}
+
+function Vec3(x, y, z) {
     return {
         x : x || 0,
         y : y || 0,
         z : z || 0,
 
         add : function(v) {
-            return Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+            return Vec3(this.x + v.x, this.y + v.y, this.z + v.z);
         },
 
         distance : function(v) {
@@ -18,7 +68,7 @@ function Vector(x, y, z) {
         },
 
         divide : function(n) {
-            return Vector(this.x / n, this.y / n, this.z / n);
+            return Vec3(this.x / n, this.y / n, this.z / n);
         },
 
         dot : function(v) {
@@ -36,18 +86,18 @@ function Vector(x, y, z) {
         normalized : function() {
             var len = this.length();
             if (len === 0.0) {
-                return Vector();
+                return Vec3();
             } else {
                 return this.divide(len);
             }
         },
 
         scale : function(n) {
-            return Vector(x * n, y * n, z * n);
+            return Vec3(x * n, y * n, z * n);
         },
 
         subtract : function(v) {
-            return Vector(this.x - v.x, this.y - v.y, this.z - v.z);
+            return Vec3(this.x - v.x, this.y - v.y, this.z - v.z);
         } 
     }
 }
