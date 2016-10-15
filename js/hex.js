9d882442163fc5ebcc92dedc874e29c6c6b8d28a
jeremyfromearth <jeremynealbrown@gmail.com>
2016-10-15 14:02:38 -0700

    Renamed coordinate getters, added hex_to_pixel method

--- js/hex.js
+++ js/hex.js
@@ -1,10 +1,54 @@
 function Hex() {
-    var FLAT = 'flat';
-    var SHARP = 'sharp'
+    
 
-    var Orientation = {
-        SHARP : 'sharp',
-        FLAT  : 'flat'
+    function Canvas(ctx) {
+        var ctx = ctx;
+        var theta = 0;
+        var flat_vertices = [];
+        var sharp_vertices = [];
+        var increment = Math.PI / 3.0;
+        var sharp_offset = increment * 0.5;
+
+        // Initialize vertices for both orientation types
+        // These are "unit" coords and can be scaled and translated as needed
+        for(var i = 0; i < 6; i++) {
+            flat_vertices.push({
+                x : Math.cos(theta),
+                y : Math.sin(theta)
+            });
+
+            sharp_vertices.push({
+                x : Math.cos(theta + sharp_offset),
+                y : Math.sin(theta + sharp_offset)
+            });
+
+            theta += increment;
+        }
+
+        return {
+            draw_hexagon : function(pixel_coords, radius, orientation, solid, stroke) {
+                if(!ctx) return; 
+
+                var vertices = orientation == 
+                    Orientation.FLAT ? flat_vertices : sharp_vertices;
+
+                ctx.beginPath();
+                ctx.moveTo(pixel_coords[0], pixel_coords[1]);
+                for(var i = 0; i < vertices.length; i++) {
+                    var vertex = vertices[i];
+                    var x = pixel_coords.x + vertex.x * radius;
+                    var y = pixel_coords.y + vertex.y * radius;
+                    if(i == 0) {
+                        ctx.moveTo(x, y);
+                    } else {
+                        ctx.lineTo(x, y);
+                    }
+                }
+                ctx.closePath();
+                if(solid) ctx.fill();
+                if(stroke) ctx.stroke();
+            }
+        }
     }
 
     function Cell(q, r, s) {
@@ -16,35 +60,35 @@ function Hex() {
             s = s || 0;
 
         return {
-            add : function(cell) {
+            plus : function(cell) {
                 return Cell(
-                    q + cell.get_q(),
-                    r + cell.get_r(),
-                    s + cell.get_s());
+                    q + cell.q(),
+                    r + cell.r(),
+                    s + cell.s());
             },
 
             distance : function(cell) {
                 return Math.floor((
-                    Math.abs(q - cell.get_q()) + 
-                    Math.abs(r - cell.get_r()) + 
-                    Math.abs(s - cell.get_s())) / 2);
+                    Math.abs(q - cell.q()) + 
+                    Math.abs(r - cell.r()) + 
+                    Math.abs(s - cell.s())) / 2);
             },
 
             equals : function(cell) { 
-                return q == cell.get_q()
-                    && r == cell.get_r()
-                    && s == cell.get_s();
+                return q == cell.q()
+                    && r == cell.r()
+                    && s == cell.s();
             },
 
-            get_q : function() { 
+            q : function() { 
                 return q; 
             }, 
 
-            get_r : function() { 
+            r : function() { 
                 return r; 
             }, 
 
-            get_s : function() { 
+            s : function() { 
                 return s; 
             }, 
 
@@ -55,11 +99,11 @@ function Hex() {
                     s * scalar);
             },
 
-            sub : function(cell) {
+            minus : function(cell) {
                 return Cell(
-                    q - cell.get_q(),
-                    r - cell.get_r(),
-                    s - cell.get_s());
+                    q - cell.q(),
+                    r - cell.r(),
+                    s - cell.s());
             }
         }
     }
@@ -99,57 +143,33 @@ function Hex() {
                 }
                 return cell_radius;
             },
+
+            hex_to_pixel : function(cell) {
+                var x = 0;
+                var y = 0;
+                switch(orientation) {
+                    case Orientation.FLAT:
+                        x = cell_radius * 3/2 * cell.q();
+                        y = cell_radius * Math.sqrt(3) * (cell.r() + cell.q() * 0.5);
+                        break;
+                    case Orientation.SHARP:
+                        x = cell_radius * Math.sqrt(3) * (cell.q() + cell.r() * 0.5);
+                        y = cell_radius * 3/2 * cell.r();
+                        break;
+                }
+
+                return {x: x, y: y};
+            }, 
+
+            pixel_to_hex : function(pixel) {
+
+            }
         }
     }
 
-    function Canvas(ctx) {
-        var ctx = ctx;
-        var theta = 0;
-        var flat_vertices = [];
-        var sharp_vertices = [];
-        var increment = Math.PI / 3.0;
-        var sharp_offset = increment * 0.5;
-
-        for(var i = 0; i < 6; i++) {
-            flat_vertices.push({
-                x : Math.cos(theta),
-                y : Math.sin(theta)
-            });
-
-            sharp_vertices.push({
-                x : Math.cos(theta + sharp_offset),
-                y : Math.sin(theta + sharp_offset)
-            });
-
-            theta += increment;
-        }
-
-        return {
-            draw_hexagon : function(pixel_coords, radius, orientation, solid, stroke) {
-                if(!ctx) return; 
-
-                var vertices = orientation == 
-                    Orientation.FLAT ? flat_vertices : sharp_vertices;
-
-                ctx.beginPath();
-                ctx.moveTo(pixel_coords[0], pixel_coords[1]);
-
-                for(var i = 0; i < vertices.length; i++) {
-                    var vertex = vertices[i];
-                    var x = pixel_coords.x + vertex.x * radius;
-                    var y = pixel_coords.y + vertex.y * radius;
-                    if(i == 0) {
-                        ctx.moveTo(x, y);
-                    } else {
-                        ctx.lineTo(x, y);
-                    }
-                }
-
-                ctx.closePath();
-                if(solid) ctx.fill();
-                if(stroke) ctx.stroke();
-            }
-        }
+    var Orientation = {
+        SHARP : 'sharp',
+        FLAT  : 'flat'
     }
 
     return {
